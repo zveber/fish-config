330971cfe73c5a82d9b47ec53180f0bb8300050d
Andrew Belitsky <belitsky.a@gmail.com>
2019-04-04 10:14:42 +0300

    print user 's name in prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -4,9 +4,9 @@ function fish_prompt
     set updated (cat /var/pacman-updates|wc -l)
     if test $updated -gt 0
         set_color red
-        echo -n "($updated)"
+        echo -n " ($updated)"
     end
     set_color normal
-    echo -n ' > '
+    echo -n " $USER> "
 end
 
