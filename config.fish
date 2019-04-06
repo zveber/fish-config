ddd3fa2ed83c825b7a82c78deb6dfc9b2f243109
Andrew Belitsky <belitsky.a@gmail.com>
2019-04-06 10:28:15 +0300

    update config

--- config.fish
+++ config.fish
@@ -1,12 +1,13 @@
-#set -U fish_user_paths /home/andrew/.local/bin $fish_user_paths
-
 # disable searching with pacman
 function __fish_command_not_found_handler --on-event fish_command_not_found
     echo "fish: Unknown command '$argv'"
 end
 
-set -x fish_color_param 001010
+# set colors
+set -x fish_color_param black
 set -x fish_color_command_error red --bold
 
+# set envuronment variables
 set -x PAGER most
+set -x fish_user_paths $HOME/.local/bin $fish_user_paths
 
