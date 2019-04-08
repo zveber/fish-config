d0b2475ef78177ca44b56d522edfdc77d30cd564
Andrew Belitsky <belitsky.a@gmail.com>
2019-04-08 22:29:38 +0300

    restore tmux session trough the config

--- config.fish
+++ config.fish
@@ -11,3 +11,8 @@ set -x fish_color_command_error red --bold
 set -x PAGER most
 set -px fish_user_paths $HOME/.local/bin
 
+# restore tmux session or start a new one
+if not set -q TMUX
+    tmux attach || tmux new
+end
+
