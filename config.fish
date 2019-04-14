19f4343383c4166e0976a7b3f9ba1e50005dd838
Andrew Belitsky <belitsky.a@gmail.com>
2019-04-14 11:58:10 +0300

    fix: export environment variables

--- config.fish
+++ config.fish
@@ -10,8 +10,8 @@ set fish_color_param black
 set fish_pager_color_completion normal --dim
 
 # set envuronment variables
-set PAGER most
-set -p fish_user_paths $HOME/.local/bin
+set -x PAGER most
+set -px fish_user_paths $HOME/.local/bin
 
 # restore tmux session or start a new one
 if not set -q TMUX
