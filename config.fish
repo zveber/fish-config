dc8c9e4402b9eda9fd321bcf174ec5b1a356de49
Andrew Belitsky <belitsky.a@gmail.com>
2019-04-14 11:31:46 +0300

    don't share variables in config

--- config.fish
+++ config.fish
@@ -4,14 +4,14 @@ function __fish_command_not_found_handler --on-event fish_command_not_found
 end
 
 # set colors
-set -x fish_color_command blue --bold
-set -x fish_color_command_error red --bold
-set -x fish_color_param black
-set -x fish_pager_color_completion normal --dim
+set fish_color_command blue --bold
+set fish_color_command_error red --bold
+set fish_color_param black
+set fish_pager_color_completion normal --dim
 
 # set envuronment variables
-set -x PAGER most
-set -px fish_user_paths $HOME/.local/bin
+set PAGER most
+set -p fish_user_paths $HOME/.local/bin
 
 # restore tmux session or start a new one
 if not set -q TMUX
