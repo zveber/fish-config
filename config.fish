ed22cbe98f5b9c9f61c572c4305930e4e7619b4c
Andrew Belitsky <belitsky.a@gmail.com>
2019-05-02 13:32:39 +0300

    update theme

--- config.fish
+++ config.fish
@@ -7,7 +7,7 @@ end
 set fish_color_command blue --bold
 set fish_color_command_error red --bold
 set fish_color_param normal
-set fish_color_completion normal --dim
+set fish_color_autosuggestion normal --dim
 
 # set envuronment variables
 set -x PAGER most
