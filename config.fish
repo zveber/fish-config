982f25cb23ee22904ace5b040244d9a7f8ab3fe9
Andrew Belitsky <belitsky.a@gmail.com>
2019-11-06 23:01:26 +0300

    don't share variables

--- config.fish
+++ config.fish
@@ -16,8 +16,8 @@ set fish_pager_color_description green --bold
 set fish_pager_color_prefix black
 
 # set envuronment variables
-set -x PAGER most
-set -px fish_user_paths $HOME/.local/bin
+set PAGER most
+set -p fish_user_paths $HOME/.local/bin
 
 # restore tmux session or start a new one
 if not set -q TMUX
