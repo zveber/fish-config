6c7ad9071c81390d695a81de0c7bbd743442f313
Andrew Belitsky <belitsky.a@gmail.com>
2020-01-01 02:45:12 +0300

    Revert "fix: nake PAGER emvironment"
    This reverts commit 481f034adf356f56b43b6c9d1e3c8e9eb9e754d6.

--- config.fish
+++ config.fish
@@ -16,7 +16,7 @@ set fish_pager_color_description green --bold
 set fish_pager_color_prefix black
 
 # set envuronment variables
-set -x PAGER most
+set PAGER most
 set -p fish_user_paths $HOME/.local/bin
 
 # restore tmux session or start a new one
